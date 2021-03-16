5fc6e0720d20dc1c66945cae756bf79a65a9d399
Stefan Dobre <svdobre@gmail.com>
2021-03-16 23:32:03 +0100

    Add files via upload

--- ocrad.js
+++ ocrad.js
@@ -43,7 +43,7 @@ var ENVIRONMENT_IS_SHELL = false;
 // 1) We could be the application main() thread running in the main JS UI thread. (ENVIRONMENT_IS_WORKER == false and ENVIRONMENT_IS_PTHREAD == false)
 // 2) We could be the application main() thread proxied to worker. (with Emscripten -s PROXY_TO_WORKER=1) (ENVIRONMENT_IS_WORKER == true, ENVIRONMENT_IS_PTHREAD == false)
 // 3) We could be an application pthread running in a worker. (ENVIRONMENT_IS_WORKER == true and ENVIRONMENT_IS_PTHREAD == true)
-
+/*
 if (Module['ENVIRONMENT']) {
   if (Module['ENVIRONMENT'] === 'WEB') {
     ENVIRONMENT_IS_WEB = true;
@@ -62,6 +62,9 @@ if (Module['ENVIRONMENT']) {
   ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'function' && !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_WORKER;
   ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIRONMENT_IS_WORKER;
 }
+*/
+
+ENVIRONMENT_IS_NODE = true;
 
 
 if (ENVIRONMENT_IS_NODE) {
